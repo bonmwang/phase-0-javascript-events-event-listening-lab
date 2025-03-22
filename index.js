3117f20eacac40c833df6010373fdb18fe7c8d40
Boniface <bm715452@gmail.com>
2025-03-22 03:25:26 +0300

    Javascript Event Listener Lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input = document.getElementById('button'); // Replace "button" with the actual ID of your button
+    input.addEventListener('click', () => {
+        alert('Button Clicked!');
+    });
 }
